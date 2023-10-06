162d14b8bcaef3ef4554016b60fb7cc37566097f
Sagar Dhaduk <sagardhaduk1@gmail.com>
2023-10-05 20:35:59 -0500

    "Created Model and API endpoint that returns view"

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import main\
+from .views import RoomView
 
 urlpatterns = [
-    path('', main)
+    path('home', RoomView.as_view())    
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
-def main(request):
-    return HttpResponse("hello")
+# Create your views here.
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+  
+
