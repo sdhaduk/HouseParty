ef5d6ced9e14ff846bec420c627c1c4839a79765
sdhaduk <sagardhaduk1@gmail.com>
2023-10-11 19:44:31 -0500

    "Added frontend components to JoinRoomPage"

--- music_controller/frontend/src/components/JoinRoomPage.js
+++ music_controller/frontend/src/components/JoinRoomPage.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  TextField,
+} from "@material-ui/core/";
+import { Link } from "react-router-dom";
 
 const JoinRoomPage = () => {
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState(false);
+
+  function handleButtonPress() {
+    console.log(roomCode)
+  }
+
+
   return (
-    <div>This is the join room page</div>
-  )
-}
+    <Grid container spacing={1} align="center">
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          error={error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={(e) => setRoomCode(e.target.value)} 
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={handleButtonPress}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default JoinRoomPage;
