9050ec6aa53d5837be054662f96528e9cca5f185
sdhaduk <sagardhaduk1@gmail.com>
2023-10-13 12:30:22 -0500

    "Styled the home page with buttons and links"

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -3,18 +3,19 @@ import {BrowserRouter, Link, Route, Routes, Redirect } from 'react-router-dom';
 import CreateRoomPage from "./CreateRoomPage";
 import JoinRoomPage from "./JoinRoomPage";
 import Room from "./Room";
+import HomePageStyle from './HomePageStyle';
 
 const HomePage = () => {
     return (
     <BrowserRouter>
         <Routes>
-            <Route index element={<h1>This is the home page</h1>}></Route>
+            <Route index element={<HomePageStyle />} />
             <Route path="/join" element={<JoinRoomPage />} />
             <Route path="/create" element={<CreateRoomPage />} />
             <Route path="/room/:roomCode" element={<Room />} />
         </Routes>
     </BrowserRouter>    
-    )
+    ) 
 };
 
 export default HomePage;
---
+++ music_controller/frontend/src/components/HomePageStyle.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import { Link } from 'react-router-dom';
+
+const HomePageStyle = () => {
+  return (
+    <Grid container spacing={3} align='center'>
+        <Grid item xs={12}>
+            <Typography variant="h3" component='h3'>
+                House Party
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+            <ButtonGroup disableElevation variant="contained" color="primary">
+                <Button color="primary" to='/join' component={Link}>Join a Room</Button>
+                <Button color="secondary" to='/create' component={Link}>Create a Room</Button>
+            </ButtonGroup>
+        </Grid>
+    </Grid>
+  )
+};
+ 
+export default HomePageStyle;
