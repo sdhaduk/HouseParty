10f1e033b41c4dbbfe0639e0b8b740d95efce250
sdhaduk <sagardhaduk1@gmail.com>
2023-10-21 00:00:39 -0500

    Added functionality that skips current song after the number of votes to skip is met

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .util import *
 from requests import Request, post
 from api.models import Room
+from spotify.models import Vote
 
 from dotenv import load_dotenv
 import os
@@ -100,8 +101,18 @@ class CurrentSong(APIView):
             'votes': 0,
             'id': song_id
         }
-
+        
+        self.update_room_song(room, song_id) 
         return Response(song, status=status.HTTP_200_OK)
+    
+    def update_room_song(self, room, song_id):
+        current_song = room.current_song
+
+        if current_song != song_id:
+            room.current_song = song_id
+            room.save(update_fields=['current_song'])
+            votes = Vote.objects.filter(room=room).delete()
+
 
 
 class PauseSong(APIView):
@@ -130,10 +141,16 @@ class SkipSong(APIView):
     def post(self, request, format=None):
         room_code = self.request.session.get('room_code')
         room = Room.objects.filter(code=room_code)[0]
-        if self.request.session.session_key == room.host:
+        votes = Vote.objects.filter(room=room, song_id=room.current_song)
+        votes_needed = room.votes_to_skip
+
+        if self.request.session.session_key == room.host or len(votes) + 1 >= votes_needed:
+            votes.delete()
             skip_song(room.host)
         else:
-            pass
+            vote = Vote(user=self.request.session.session_key, room=room, song_id=room.current_song)
+            vote.save()
+            
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 
